9d1e243a892bcc436a5dad656dca19542987a538
Jay <jayzawrotny@gmail.com>
2020-10-13 22:05:36 -0400

    Added docs to finish up the cleanup

--- src/main/cljs_news/app.cljs
+++ src/main/cljs_news/app.cljs
@@ -8,11 +8,30 @@
 (def news (r/atom []))
 (def selected-article (r/atom nil))
 
+(defn reset-async!
+  "
+  Takes an atom and a promise.
+  Resets the contents of the atom to the results of the promise on resolve.
+  Returns a promise.
+  "
+  [an-atom promise]
+  (p/let [res promise]
+    (pprint res)
+    (reset! an-atom res)))
+
 (defn json->clj
+  "
+  Takes js or parsed JSON data.
+  Returns a cljs hash-map with :keyword keys
+  "
   [json-data]
   (js->clj json-data :keywordize-keys true))
 
 (defn fetch-news
+  "
+  Fetches pre-baked JSON data from the local server
+  Returns a promise resolving a list of the 8 latest articles from the JSON.
+  "
   []
   (p/->> (js/fetch "/clojure-news.json")
          (.json)
@@ -21,18 +40,31 @@
          (take 8)))
 
 (defn articles-view
+  "
+  Sidebar displaying a list of articles. Clicking on title selects an article.
+  Takes a list of article hash-maps to render
+  Returns a nav element in hiccup form
+  "
   [articles]
   [:nav.articles
    [:ul.articles-list
     (for [article articles]
-     [:li.articles-list__item
-      {:key (:publishedAt article)}
-      [:button.articles-list__button
-       {:on-click #(reset! selected-article article)}
-       [:h2.articles-list__title (:title article)]
-       [:date.articles-list__date (:publishedAt article)]]])]])
+      [:li.articles-list__item
+       {:key (:publishedAt article)}
+       [:button.articles-list__button
+        {:on-click #(reset! selected-article article)}
+        [:h2.articles-list__title (:title article)]
+        [:date.articles-list__date (:publishedAt article)]]])]])
 
 (defn summary-view
+  "
+  Displays details about the selected article. Displays a prompt if no article
+  is selected.
+
+  Takes an article hash-map
+  Returns an article hiccup element with article summary content or prompt
+  paragraph if no article is selected.
+  "
   [article]
   [:article.article
    (if-not article
@@ -53,6 +85,10 @@
             :target "_blank"} (:url article)]]])])
 
 (defn news-view
+  "
+  Base view for our news UI. Reads the list of news articles and currently
+  selected article from our stateful atoms.
+  "
   []
   (let [articles @news
         article @selected-article]
@@ -61,18 +97,21 @@
      [summary-view article]]))
 
 (defn app
+  "
+  Root view.
+  Displays loading news text until there's at least one article.
+  "
   []
   (if (= (count @news) 0)
     [:div.loading "Loading news..."]
     [news-view]))
 
-(defn reset-async!
-  [an-atom promise]
-  (p/let [res promise]
-    (pprint res)
-    (reset! an-atom res)))
 
 (defn ^:dev/after-load init
+  "
+  App entrypoint. Runs after loading updated code in development mode.
+  Mounts the root app view element to a <div id=\"app\">...</div>
+  "
   []
   (reset-async! news (fetch-news))
   (rd/render [app] (js/document.getElementById "app") ))
